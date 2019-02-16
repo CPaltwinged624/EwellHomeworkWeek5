dcadc4274ab3f40b3f52d2b93376427cebf60f0d
AZ <me403016@ohio.edu>
2019-02-16 16:18:42 -0500

    upload project

---
+++ Assets/PlayerDetector.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDetector : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetMouseButtonDown (0))
+        {
+            Ray cameraRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(cameraRay, out hit))
+            {
+                if (Physics.Raycast(transform.position, -Vector3.up, out hit))
+                    print("Cube");
+            }
+            Debug.Log(hit.point);
+                print("NOTHINGAHEAD");
+            }
+        }
+    }
+
---
+++ Assets/Playermotor.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Playermotor : MonoBehaviour
+{
+    public GameObject player;
+    public Transform destinationPosition;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += Vector3.forward * Time.deltaTime;
+
+     
+    }
+}
